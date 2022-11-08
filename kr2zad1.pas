54c6a0fea58885d982f8fad8db051acf422195c0
neganfox228 <117731124+neganfox228@users.noreply.github.com>
2022-11-08 16:50:36 +0300

    Add files via upload

---
+++ kr2zad1.pas
@@ -0,0 +1,22 @@
+﻿
+var i2: integer;
+var p:array[1..9999] of integer;
+   var v: integer;
+   begin
+     for var i:=1 to 10 do
+       begin
+      p[i]+=Random(-100,100);
+      Print(p[i]+3,'  ');
+   end;
+   PrintLN;
+   v+=1;
+   i2+=2;
+   For var i:=1 to 9 do
+      For var j:=i+1 to 10 do
+         If Abs(p[i]-p[j])>Abs(p[v]-p[i2]) then
+         begin
+            v:=i;
+            i2:=j;
+         end;
+   Print('|p[',v,'] - p[',i2,']| = ',Abs(p[v]-p[i2]));
+end.
---
+++ kr2zad2.pas
@@ -0,0 +1,55 @@
+﻿begin
+var k, v, h, b: integer;
+var g: string;
+println('vvedite stroku');
+var s := ReadString;
+println('vvedite podstroku');
+var c := ReadString;
+var i := 1;
+while (i <= length(s)) do
+begin
+k := 1;
+if s[i] = c[1] then
+begin
+k := 1;
+v := i;
+h := 1;
+while(k <= length(c)) do
+begin
+if s[v] = c[k] then
+begin
+inc(v);
+inc(k);
+inc(h);
+end
+else
+begin
+g := g + s[i];
+break;
+end;
+end;
+if h = length(c) + 1 then
+begin
+k := i;
+b := length(c) + i - 1;
+while (b >= k) do
+begin
+g := g + s[b];
+b := b - 1;
+end;
+i := v;
+if length(c) + k < length(s) then
+g := g + s[i];
+end
+else
+begin
+i := v;
+g := g + s[i];
+end
+end
+else
+g := g + s[i];
+inc(i);
+end;
+println(g);
+end.
